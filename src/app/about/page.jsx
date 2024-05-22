68cf1a2533cb19666f52cce44066c139385523ae
a9na <anaiscoding@gmail.com>
2024-05-22 16:31:58 +0200

    commit

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -14,6 +14,7 @@ const AboutPage = () => {
   const experienceRef = useRef();
   const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" });
 
+
   return (
     <motion.div
       className="h-full"
@@ -26,29 +27,28 @@ const AboutPage = () => {
           <div className="flex flex-col gap-12 justify-center">
             <Image
               src="/my_photo.jpeg"
-              alt=""
+              alt="My Photo"
               width={112}
               height={112}
               className="w-28 h-28 rounded-full object-cover"
             />
             <h1 className="font-bold text-2xl">About Me</h1>
             <p className="text-lg">
-            An ambitious and dedicated student at FERIT Osijek with a firm drive and enthusiasm 
-            that fuels my pursuit of complex challenges, while uplifting and inspiring my colleagues and team.
+              An ambitious and dedicated student at FERIT Osijek with a firm drive and enthusiasm
+              that fuels my pursuit of complex challenges, while uplifting and inspiring my colleagues and team.
             </p>
             <p>
-            I am curently working as a private tutor, giving lessons to students in the area of 
-            mathematics, physics, programming (C, Javascript) as well as information technology 
-            and computer networks.
+              I am currently working as a private tutor, giving lessons to students in the area of
+              mathematics, physics, programming (C, JavaScript) as well as information technology
+              and computer networks.
             </p>
             <p>
-            Also very actively volounteering in organizations as one of my main 
-            life principles is: "The meaning of life is to find your gift. 
-            The purpose of life is to give it away." 
+              Also very actively volunteering in organizations as one of my main
+              life principles is: "The meaning of life is to find your gift.
+              The purpose of life is to give it away."
             </p>
-
             <span className="italic">
-            Open for full-stack development and software engineering roles.
+              Open for full-stack development and software engineering roles.
             </span>
           </div>
           <div className="flex flex-col gap-12 justify-center" ref={skillRef}>
@@ -64,9 +64,8 @@ const AboutPage = () => {
               initial={{ x: "-300px" }}
               animate={isSkillRefInView ? { x: 0 } : {}}
               className="flex gap-2 flex-wrap"
-            >            
-            <div>Languages: </div> 
-
+            >
+              <div>Languages:</div>
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 C
               </div>
@@ -76,15 +75,13 @@ const AboutPage = () => {
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 JAVASCRIPT
               </div>
-            
             </motion.div>
-
             <motion.div
               initial={{ x: "-300px" }}
               animate={isSkillRefInView ? { x: 0 } : {}}
               className="flex gap-2 flex-wrap"
-            >            
-            <div>Technologies: </div> 
+            >
+              <div>Technologies:</div>
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 VUE.JS
               </div>
@@ -124,28 +121,26 @@ const AboutPage = () => {
               <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
                 MYSQL
               </div>
-              </motion.div>
-
-              <motion.div
+            </motion.div>
+            <motion.div
               initial={{ x: "-300px" }}
               animate={isSkillRefInView ? { x: 0 } : {}}
               className="flex gap-2 flex-wrap"
-            >  
-              <div>Workflow: </div> 
-            <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-              Visual Studio
-            </div>
-            <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-              Visual Studio Code
-            </div>
-            <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-              Linux
-            </div>
-            <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
-              Git
-            </div>
+            >
+              <div>Workflow:</div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Visual Studio
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Visual Studio Code
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Linux
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-black text-white hover:bg-white hover:text-black">
+                Git
+              </div>
             </motion.div>
-
             <motion.svg
               initial={{ opacity: 0.2, y: 0 }}
               animate={{ opacity: 1, y: "10px" }}
@@ -162,17 +157,12 @@ const AboutPage = () => {
                 strokeWidth="1"
               ></path>
               <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
-              <path
-                d="M15 11L12 14L9 11"
-                stroke="#000000"
-                strokeWidth="1"
-              ></path>
+              <path d="M15 11L12 14L9 11" stroke="#000000" strokeWidth="1"></path>
             </motion.svg>
           </div>
-          <div
-            className="flex flex-col gap-12 justify-center pb-408"
-            ref={experienceRef}
-          >
+
+
+          <div className="flex flex-col gap-12 justify-center pb-48" ref={experienceRef}>
             <motion.h1
               initial={{ x: "-300px" }}
               animate={isExperienceRefInView ? { x: "0" } : {}}
@@ -186,32 +176,70 @@ const AboutPage = () => {
               animate={isExperienceRefInView ? { x: "0" } : {}}
               className=""
             >
-              <div className="flex">
-                <div className="w-1/3 ">
+              <div className="flex justify-between h-82">
+                <div className="w-1/3">
                   <div className="bg-white p-3 font-semibold rounded-b-lg rounded-s-lg">
                     Private Tutor
                   </div>
-+                  <div className="p-3 text-sm italic">
-                <div>
-                  •Giving lessons to students in the area of mathematics, physics,
-                   programming (C, JavaScript) as well as information technology and computer networks
-                </div> 
-                <br></br>
-                <div>
-                  •	Demonstrated leadership skills by effectively guiding and motivating students 
-                  to achieve academic success, fostering a supportive learning environment,
-                  and providing mentorship to encourage personal and intellectual growth
-                </div>{" "}
+                  <div className="p-3 text-sm italic">
+                    <div>
+                      • Giving lessons to students in the area of mathematics, physics,
+                      programming (C, JavaScript) as well as information technology and computer networks
+                    </div>
+                    <br />
+                    <div>
+                      • Demonstrated leadership skills by effectively guiding and motivating students
+                      to achieve academic success, fostering a supportive learning environment,
+                      and providing mentorship to encourage personal and intellectual growth
+                    </div>
                   </div>
                   <div className="p-3 text-red-400 text-sm font-semibold">
-                  Aug 2023 - Present 
+                    Aug 2023 - Present
                   </div>
                 </div>
               </div>
             </motion.div>
           </div>
-        </div>
-      </div>
+
+          <div className="flex flex-col gap-12 justify-between h-82">            
+          <h1 className="font-bold text-2xl">
+              EDUCATION
+            </h1>
+              <h2>Faculty of Electrical Engineering and Computer Science Osijek</h2>
+              <p className="text-lg">
+                <div className="font-bold">Bachelor of technology in Computer Science</div>
+                Relevant courses: Programming in Java; Data Structures and Algorithms; Information Systems 
+                and Computer Networks; Operating Systems; Database Management Systems
+              </p>
+              <h2>CS50</h2>
+              <p className="text-lg">
+                <div className="font-bold">Harvard University's introduction to the intellectual enterprises of computer science and 
+                the art of programming.</div>
+                Languages include C, Python, SQL, and JavaScript as well as CSS and HTML.
+              </p>
+          </div>
+
+          <div className="flex flex-col gap-12 justify-between h-82">            
+            <h1 className="font-bold text-2xl">HOBBIES & INTERESTS</h1>
+            <div className="p-4 text-sm cursor-pointer bg-black text-white">
+              Volunteering
+            </div>
+            <div className="p-4 text-sm cursor-pointer bg-black text-white">
+              Language learning
+              <div>I speak English (C2), German (B2) and Italian (B2)</div>
+            </div>
+            <div className="p-4 text-sm cursor-pointer bg-black text-white">
+              Sports 
+              <div>Volleyball, Running, Mountain climbing, Gym</div>
+            </div>
+            <div className="p-4 text-sm cursor-pointer bg-black text-white">
+              Reading and Writing 
+              <div>I write reviews regularly at <a href="https://www.goodreads.com/user/show/101703919-ana">Goodreads</a></div>
+              <div>Also posting financial advice at <a href="https://www.instagram.com/fiscus.cro/">Instagram</a></div>
+            </div>
+            </div>
+          </div>
+        </div>          
     </motion.div>
   );
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ const Homepage = () => {
       transition={{ duration: 1 }}
     >
       <div className="h-full flex flex-col lg:flex-row px-12 sm:px-8 md:px-12 lg:px-20 xl:px-48">
-        <div className="flex items-center justify-center h-1/4 md:h-1/2 lg:w-1/4 relative rounded-lg overflow-hidden mx-auto">
+      <div className="h-1/2 lg:h-full lg:w-1/2 relative">
           <Image src="/my_photo.jpeg" alt="" fill className="object-contain" />
         </div>
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
