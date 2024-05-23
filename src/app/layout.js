8ab66757b39feb8f0142a5167ab7ea28c8927c37
a9na <anaiscoding@gmail.com>
2024-05-23 15:33:03 +0200

    commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TransitionProvider from "@/components/transitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <TransitionProvider>{children}</TransitionProvider>
+        <transitionProvider>{children}</transitionProvider>
       </body>
     </html>
   );
